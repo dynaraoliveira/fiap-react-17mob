c5437ea49eb603427f68fdafd91535da95ad763c
Usuário Convidado <Guest@PA06MICRO07.local>
2019-05-22 22:52:58 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Button, View } from 'react-native';
+
+import Seasons  from './components/Seasons';
+
+import { SafeAreaView } from 'react-navigation';
+
+export default class App extends React.Component {
+  getData(season) {
+    fetch(`http://ergast.com/api/f1/${season}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Seasons handleClick={ this.getData } />
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
